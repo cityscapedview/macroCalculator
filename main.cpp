ff33bbff87763598eedd596c002c34e06342d78a
David Fagan <davida.fagan@gmail.com>
2025-02-18 08:03:58 -0800

    Added project to do list in main.
    Maybe not the best practice, but it is easier to have the list right
    there in the code.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "User.h"
 
+// TODO:
+// - [ ] Abstract all std in & out to main.cpp class for now. (can refactor later).
+// - [ ] Instantiate user on startup.
+// - [ ] Use setters to set data with try and catch logic in users
+// - [ ] Handle all business logic in Users
+// - [ ] Finish populating functions for calculating.
+
+
 int main()
 {
   // User user = User::createUser();
